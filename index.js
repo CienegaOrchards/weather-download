0064f666b412213eea5a47844c41a5bdb5cc7e5b
Craig R. Hughes <craig.git@rungie.com>
2017-10-24 10:59:31 -0700

    Add throttling and progress

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const promisify = require('es6-promisify');
 const _ = require('lodash');
+const throat = require('throat');
+const ProgressBar = require('progress');
 
 const moment = require('moment');
 require('twix');
@@ -10,6 +12,11 @@ const stringify = promisify(require('csv-stringify'));
 const request = require('request-promise-native');
 
 // https://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KCAHOLLI28&graphspan=day&month=2&day=16&year=2016&format=1
+
+// const range = moment('2016-02-16').twix('2016-02-17', { allDay: true }).toArray('days');
+const range = moment('2016-02-16').twix(moment(), { allDay: true }).toArray('days');
+
+const bar = new ProgressBar(' downloading [:bar] :rate/rps :percent :etas', { total: range.length,  width: 100 });
 
 const fetch_date = function (date) {
     return request({
@@ -24,6 +31,11 @@ const fetch_date = function (date) {
         },
     })
     .then((body) => {
+        bar.tick();
+        if (bar.complete) {
+            console.error('\ncomplete\n');
+        }
+
         return body.replace(/,\n<br>\n/gi, '\n').replace('<br>','');
     })
     .catch((err) => {
@@ -31,11 +43,8 @@ const fetch_date = function (date) {
     });
 }
 
-// const range = moment('2016-02-16').twix('2016-02-17', { allDay: true }).toArray('days');
-const range = moment('2016-02-16').twix(moment(), { allDay: true }).toArray('days');
-
 // console.log(range.map(date => ({day:date.date(), month:date.month(), year:date.year()})));
-Promise.all(range.map(date => fetch_date(date)))
+Promise.all(range.map(throat(25, date => fetch_date(date))))
 .then(csv_results => {
     return Promise.all(csv_results.map(csv_data => parse(csv_data, { auto_parse: true, columns: true, skip_empty_lines: true })));
 })
